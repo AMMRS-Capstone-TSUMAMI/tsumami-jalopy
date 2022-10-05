e412c97d2d3351cd9ea5cfa7ed0604bc8029c232
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-05 12:09:45 -0500

    completed macro allocation class

--- src/main/java/tsumami/tsumamijalopy/data/MacroAllocation.java
+++ src/main/java/tsumami/tsumamijalopy/data/MacroAllocation.java
@@ -1,4 +1,34 @@
 package tsumami.tsumamijalopy.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="macro_allocations")
+
 public class MacroAllocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    public String dietType;
+
+    @NotNull
+    public int carbPercent;
+
+    @NotNull
+    public int fatPercent;
+
+    @NotNull
+    public int proteinPercent;
 }
