d772fcc4b307cbcd2a23340522615b6a9dc168a6
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-05 14:49:00 -0500

    plan week class

--- src/main/java/tsumami/tsumamijalopy/data/PlanWeek.java
+++ src/main/java/tsumami/tsumamijalopy/data/PlanWeek.java
@@ -1,4 +1,21 @@
 package tsumami.tsumamijalopy.data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
 public class PlanWeek {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Transient
+    @NotEmpty
+//    @Column(length = 100)
+    private Date start_date;
+
+
 }
