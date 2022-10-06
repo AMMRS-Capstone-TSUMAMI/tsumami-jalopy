de4a0025a46702f138dfe438e16ca6d663114285
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-05 21:37:27 -0500

    nutrient populating

--- src/main/java/tsumami/tsumamijalopy/data/Nutrient.java
+++ src/main/java/tsumami/tsumamijalopy/data/Nutrient.java
@@ -1,4 +1,23 @@
 package tsumami.tsumamijalopy.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="nutrient")
+@Table
+
 public class Nutrient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
 }
