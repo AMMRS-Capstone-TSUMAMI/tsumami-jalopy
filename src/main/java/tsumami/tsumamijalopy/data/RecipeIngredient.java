ff7deda05fcbab77c15404bc227dbc3f321c7110
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-05 22:27:06 -0500

    recipe ingredient populating

--- src/main/java/tsumami/tsumamijalopy/data/RecipeIngredient.java
+++ src/main/java/tsumami/tsumamijalopy/data/RecipeIngredient.java
@@ -1,4 +1,28 @@
 package tsumami.tsumamijalopy.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="recipeIngredient")
+@Table
+
 public class RecipeIngredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty
+    private double amount;
+
+    @NotEmpty
+    private long unit;
 }
