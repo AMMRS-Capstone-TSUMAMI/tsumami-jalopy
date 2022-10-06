9bf697ebd07e0c904b1f54b4dc0c00da679c63bd
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-10-05 20:04:47 -0500

    intolerance basic functionality

--- src/main/java/tsumami/tsumamijalopy/data/IntolerancesRepository.java
+++ src/main/java/tsumami/tsumamijalopy/data/IntolerancesRepository.java
@@ -1,4 +1,14 @@
 package tsumami.tsumamijalopy.data;
 
-public interface IntolerancesRepository {
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import tsumami.tsumamijalopy.data.Category;
+//import tsumami.tsumamijalopy.data.data.User;
+//import tsumami.tsumamijalopy.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntolerancesRepository extends JpaRepository<Intolerance, Long> {
+//    IntolerancesRepository name(String name);
+
 }
+
