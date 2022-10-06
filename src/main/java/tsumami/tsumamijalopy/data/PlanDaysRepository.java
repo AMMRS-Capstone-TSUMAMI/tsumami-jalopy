7964a40813199e52727550a15294d99fb63404e0
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-05 21:00:24 -0500

    plan day populating

--- src/main/java/tsumami/tsumamijalopy/data/PlanDaysRepository.java
+++ src/main/java/tsumami/tsumamijalopy/data/PlanDaysRepository.java
@@ -1,4 +1,7 @@
 package tsumami.tsumamijalopy.data;
 
-public interface PlanDaysRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanDaysRepository extends JpaRepository<PlanDay, Long> {
+
 }
