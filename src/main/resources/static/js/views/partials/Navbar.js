8c8293ace1bb5634274e347524a62ce7c54c5412
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-10-07 18:07:53 -0500

    removed landing page link and reordered other links

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -4,11 +4,10 @@ export default function Navbar(props) {
     if(isLoggedIn()) {
         return `
             <nav class="d-flex bd-highlight mb-3 mt-1">
+                <a href="/about" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Logo(About)<a/>
                 <a href="/meals" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Meals</a>
                 <a href="/me" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Me</a>
-                <a href="/about" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Logo(About)<a/>
                 <a href="/logout" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Logout<a/>
-                <a href="/landing" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Landing<a/>
             </nav>
     `;
     } else {
