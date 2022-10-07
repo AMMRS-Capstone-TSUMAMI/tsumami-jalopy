2ecbf9149f41b87b8c6958262791d6bba3061cbd
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-06 21:23:38 -0500

    plan timeslot recipe associative table

--- src/main/java/tsumami/tsumamijalopy/data/PlanTimeslot.java
+++ src/main/java/tsumami/tsumamijalopy/data/PlanTimeslot.java
@@ -1,5 +1,6 @@
 package tsumami.tsumamijalopy.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Collection;
 
 @Getter
 @Setter
@@ -19,6 +21,20 @@ public class PlanTimeslot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.REFRESH},
+            targetEntity = Recipe.class)
+    @JoinTable(
+            name="plan_timeslot_recipe",
+            joinColumns = {@JoinColumn(name = "plan_timeslot_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="recipe_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
+    @JsonIgnoreProperties("plan_timeslots")
+    private Collection<Recipe> recipes;
 
     @NotEmpty
     private String timeslot;
