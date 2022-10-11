d5f7d13677887808e8d9c971a96e83ab85471ec4
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-11 11:13:19 -0500

    about css class changes

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -4,25 +4,27 @@ export default function Home(props) {
     console.log("The frontend did it. HER FAULT");
     return `
         <main>
-            <section>
-                <div class="container">
-                    <h1 class="brand">Search Recipe</h1>
-                    <form>
-                        <input type="text" placeholder="Search Your Recipe...">
-                        <i class="fa-solid fa-magnifying-glass search-icon"></i>
-                    </form>
-                    <div class="search-result">
-                        <div class="item">
-                            <img src="../../img/food_landing.jpeg" alt="">
-                            <div class="flex-container">
-                                <h1 class="title">This is a recipe</h1>
-                                <a href="#">View Recipe</a>
+            <div class="home-container">
+                <section>
+                    <div class="h-container">
+                        <h1 class="home-brand">Search Recipe</h1>
+                        <form>
+                            <input type="text" placeholder="Search Your Recipe...">
+                            <i class="fa-solid fa-magnifying-glass search-icon"></i>
+                        </form>
+                        <div class="home-search-result">
+                            <div class="home-item">
+                                <img src="../../img/food_landing.jpeg" alt="">
+                                <div class="home-flex-container">
+                                    <h1 class="home-title">This is a recipe</h1>
+                                    <a href="#">View Recipe</a>
+                                </div>
+                                <p class="home-item-data">Calories: 120</p>
                             </div>
-                            <p class="item-data">Calories: 120</p>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </main>
     `;
 }
