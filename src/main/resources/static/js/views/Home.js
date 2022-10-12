36b52ab74656bf37671d1781d4caddd8707ca085
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-12 16:35:47 -0500

    search functionality

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -1,34 +1,5 @@
 // import styles from "../../css/home.css";
 
-export default function Home(props) {
-    console.log("The frontend did it. HER FAULT");
-    return `
-        <main>
-            <div class="home-container">
-                <section>
-                    <div class="h-container">
-                        <h1 class="home-brand">Search Recipe</h1>
-                        <form>
-                            <input type="text" class="search-input" id="search-input" placeholder="Search Your Recipe...">
-                            <i class="fa-solid fa-magnifying-glass search-icon" id="search-icon"></i>
-                        </form>
-                        <div class="home-search-result">
-                            <div class="home-item">
-                                <img src="../../img/food_landing.jpeg" alt="">
-                                <div class="home-flex-container">
-                                    <h1 class="home-title">This is a recipe</h1>
-                                    <a href="#">View Recipe</a>
-                                </div>
-                                <p class="home-item-data">Calories: 120</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </main>
-    `;
-}// import styles from "../../css/home.css";
-
 let recipes = []
 
 export default function Home(props) {
