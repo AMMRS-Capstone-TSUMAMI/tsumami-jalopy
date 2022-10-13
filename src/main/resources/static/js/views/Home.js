ddf650cd1211d3424cae14dfa102763e24419f2d
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-13 08:58:14 -0500

    search functionality connected to api

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -1,5 +1,4 @@
 
-let recipes = []
 
 export default function Home(props) {
     console.log("The frontend did it. HER FAULT");
@@ -7,84 +6,64 @@ export default function Home(props) {
     return `
         <main>
             <div class="home-container">
-                <div class="h-container">
-                    <h1 class="home-brand">Search Recipe</h1>
-                    <form>
-                        <input type="text" id="search-bar" placeholder="Search Your Recipe...">
-                        <i class="fa-solid fa-magnifying-glass search-icon"></i>
-                    </form>              
-                </div>
+                <h1 class="home-brand">Search Recipe</h1>
+                <form>
+                    <input type="text" id="search-bar" placeholder="Search Your Recipe...">
+                    <i class="fa-solid fa-magnifying-glass search-icon"></i>
+                </form>
+                <div id="search-results"></div>
             </div>
         </main>
     `;
 }
 
-// export async function getAPI() {
-// //     return fetch(``).then(resp => return resp.json())
-// //
-//     const request = {
-//         method: "GET",
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }
-//     return await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=d2ecbe1149a34180a752204407a10c5b&query=chicken&number=2`, request)
-//         .then(function(response) {
-//             if(!response.ok) {
-//                 console.log("Error Finding Recipe: " + response.status);
-//             } else {
-//                 console.log("Search Complete");
-//                 console.log(response.json());
-//                 console.log(response.json()[0]);
-//                 console.log(response.json()[0].title);
-//                 return response.json();
-//             }
-//         });
-// }
-//
-//
-// function searchBar(e){
-//
-// //    add event listener to search bar
-// //    when event is fired
-// //    build a filtered list of movies based on input
-// //    update Gallery Div to new HTML
-// //    html = searched list
-// //     console.log(movies)
-//
-//     let searchMovie = document.getElementById('search-bar');
-//
-//     searchMovie.addEventListener('keyup', function () {
-//         // e.preventDefault();
-//         let userInput = this.value.toLowerCase();
-//         let filteredRecipes = [];
-//
-//         recipes.forEach(function(recipe) {
-//             if (recipe.title.toLowerCase().includes(userInput)) {
-//                 filteredRecipes.push(recipe);
-//             }
-//         });
-//
-//         let html = "";
-//         const recipesContainer = document.querySelector("#recipes-container");
-//
-//         filteredRecipes.forEach(function(recipe) {
-//             html += `
-//             <div class="recipe-card">
-//                 <div class="card-image">
-//                     <img src="${recipe.image}" width="25px" height="auto" class="card-img" alt="recipe-image">
-//                 </div>
-//
-//                 <div class="card-body">
-//                     <h3 class="card-title">${recipe.title}</h3>
-//
-//                 </div>
-//             </div>
-//            `;
-//         });
-//
-//         recipesContainer.innerHTML = html;
-//
-//     });
-// }
-// searchBar();
+export function HomeEvents() {
+    searchBarHandler()
+}
+
+
+function searchBarHandler(e) {
+    const userInput = document.querySelector('#search-bar');
+
+    userInput.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            let userInput2 = userInput.value;
+            getAPI(userInput2);
+        }
+    });
+
+    function getAPI(userSearch) {
+        return fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=d2ecbe1149a34180a752204407a10c5b&query=${userWant}&number=5`).then(resp => resp.json()).then(food => {
+            let recipeArray = [];
+            for (let i = 0; i < 5; i++) {
+                recipeArray.push(food.results[i])
+            }
+            console.log(recipeArray)
+            let html = "";
+            recipeArray.forEach(function (recipe, index) {
+                html += `
+            <h3 food-data-id="${recipe.id}" classname="card-title">${recipe.title}</h3>`
+            })
+            const recipesContainer = document.querySelector("#search-results");
+            recipesContainer.innerHTML = html;
+
+            //dont delete this
+        }).catch(erre => console.log(erre));
+    }
+
+// id
+//     :
+//     637876
+// image
+//     :
+//     "https://spoonacular.com/recipeImages/637876-312x231.jpg"
+// imageType
+//     :
+//     "jpg"
+// title
+//     :
+//     "Chicken 65"
+//         [[Prototype]]
+// :
+// Object
+}
