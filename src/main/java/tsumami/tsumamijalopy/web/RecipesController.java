09d9e33400fbc2b14a27a9f0ac33b039723c2a54
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-10-13 17:15:56 -0500

    added create endpoint

--- src/main/java/tsumami/tsumamijalopy/web/RecipesController.java
+++ src/main/java/tsumami/tsumamijalopy/web/RecipesController.java
@@ -1,4 +1,20 @@
 package tsumami.tsumamijalopy.web;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tsumami.tsumamijalopy.data.Recipe;
+import tsumami.tsumamijalopy.data.RecipesRepository;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(value = "/api/recipes", produces = "application/json")
 public class RecipesController {
+    private RecipesRepository recipesRepository;
+    @PostMapping("/create")
+    public void createRecipe(@RequestBody Recipe newRecipe) {
+        recipesRepository.save(newRecipe);
+    }
 }
