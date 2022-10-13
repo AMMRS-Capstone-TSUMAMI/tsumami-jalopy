40737c3ceb0c7575d62e4f3656b54995a12fac35
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-10-13 16:06:43 -0500

    added drag and drop capability

--- src/main/resources/static/js/views/Meals.js
+++ src/main/resources/static/js/views/Meals.js
@@ -15,20 +15,21 @@ export default function Meals(props) {
             </div>
         </div>
     </div>
-    <div id="meals-main" class="container g-5">
+    <div id="meals-main" class="container g-0">
         <div class="row">
-            <div class="col-2">
+            <div class="col-2 gs-0 ge-3">
                 <div id="meals-search-recipes" class="row">
-                    <div class="col">
-                    <form>
-                        <div class="mb-3">
-                            <label for="meals-recipe-search" class="form-label">Search Recipes</label>
-                            <input type="search" class="form-control" id="meals-recipe-search" placeholder="Ex. Keto Sushi">
-                        </div>
-                         <div id="recipe-button" class="mb-3">
-                            <button class="btn btn-secondary d-none">Search</button>
-                        </div>
-                    </form>
+                    <div class="col px-0">
+                        <div></div>
+                        <form>
+                            <div class="mb-3">
+                                <label for="meals-recipe-search" class="form-label">Search Recipes</label>
+                                <input type="search" class="form-control" id="meals-recipe-search" placeholder="Ex. Keto Sushi">
+                            </div>
+                             <div id="recipe-button" class="mb-3">
+                                <button class="btn btn-secondary d-none">Search</button>
+                            </div>
+                        </form>
                      <form>
                         <div class="mb-3">
                             <label for="meals-favorite-recipe-search" class="form-label">Search Favorites</label>
@@ -43,64 +44,64 @@ export default function Meals(props) {
                     </div>
                 </div>
             </div>
-            <div class="col-10">
+            <div class="col-10 gs-3 ge-0">
                 <div id="meals-calendar" class="row">
-                    <div class="col">
+                    <div class="col ps-1 pe-0">
                         <ul>
                             <li class="timeslot-name"></li>
-                            <li class="meals-calendar-border">Monday</li>
-                            <li class="meals-calendar-border">Tuesday</li>
-                            <li class="meals-calendar-border">Wednesday</li>
-                            <li class="meals-calendar-border">Thursday</li>
-                            <li class="meals-calendar-border">Friday</li>
-                            <li class="meals-calendar-border">Saturday</li>
-                            <li class="meals-calendar-border">Sunday</li>
+                            <li class="meals-calendar">Monday</li>
+                            <li class="meals-calendar">Tuesday</li>
+                            <li class="meals-calendar">Wednesday</li>
+                            <li class="meals-calendar">Thursday</li>
+                            <li class="meals-calendar">Friday</li>
+                            <li class="meals-calendar">Saturday</li>
+                            <li class="meals-calendar">Sunday</li>
                         </ul>
                         <ul class="meals-calendar-row">
                             <li class="timeslot-name morning">
                                 <i class="bi bi-brightness-alt-high-fill"></i>
                             </li>
-                            <li class="meals-calendar-border" id="day1-slot1">
+                            <li class="meals-calendar timeslot" id="day1-slot1">
 <!--                                id = 649280-->
 <!--                                title = "Lasagna Silvia"-->
 <!--                                image = "https://spoonacular.com/recipeImages/649280-312x231.jpg"-->
-                                    <div class="card meal-card" style="background-image: url(https://spoonacular.com/recipeImages/649280-312x231.jpg)">
+                                    <div class="card meal-card" id="649280" draggable="true" style="background-image: url(https://spoonacular.com/recipeImages/649280-312x231.jpg)">
                                         <div class="card-body"></div>
                                         <div class="card-footer">Lasagna Silvia</div>
                                     </div>
 <!--                                    <div class="recipe-card" style="background-image: url(https://spoonacular.com/recipeImages/649280-312x231.jpg)">Lasagna Silvia</div>-->
 <!--                                    <div class="recipe-card" style="background-image: url(https://spoonacular.com/recipeImages/649280-312x231.jpg)">Lasagna Silvia</div>-->
                             </li>
-                            <li class="meals-calendar-border" id="day2-slot1"></li>
-                            <li class="meals-calendar-border" id="day3-slot1"></li>
-                            <li class="meals-calendar-border" id="day4-slot1"></li>
-                            <li class="meals-calendar-border" id="day5-slot1"></li>
-                            <li class="meals-calendar-border" id="day6-slot1"></li>
-                            <li class="meals-calendar-border" id="day7-slot1"></li>
+                            <li class="meals-calendar timeslot" id="day2-slot1"></li>
+                            <li class="meals-calendar timeslot" id="day3-slot1"></li>
+                            <li class="meals-calendar timeslot" id="day4-slot1"></li>
+                            <li class="meals-calendar timeslot" id="day5-slot1"></li>
+                            <li class="meals-calendar timeslot" id="day6-slot1"></li>
+                            <li class="meals-calendar timeslot" id="day7-slot1"></li>
                         </ul>
                         <ul class="meals-calendar-row">
                             <li class="timeslot-name noon">
                                 <i class="bi bi-brightness-high-fill"></i>
                             </li>
-                            <li class="meals-calendar-border" id="day1-slot2"></li>
-                            <li class="meals-calendar-border" id="day2-slot2"></li>
-                            <li class="meals-calendar-border" id="day3-slot2"></li>
-                            <li class="meals-calendar-border" id="day4-slot2"></li>
-                            <li class="meals-calendar-border" id="day5-slot2"></li>
-                            <li class="meals-calendar-border" id="day6-slot2"></li>
-                            <li class="meals-calendar-border" id="day7-slot2"></li>
+                            <li class="meals-calendar timeslot" id="day1-slot2"></li>
+                            <li class="meals-calendar timeslot" id="day2-slot2"></li>
+                            <li class="meals-calendar timeslot" id="day3-slot2"></li>
+                            <li class="meals-calendar timeslot" id="day4-slot2"></li>
+                            <li class="meals-calendar timeslot" id="day5-slot2"></li>
+                            <li class="meals-calendar timeslot" id="day6-slot2"></li>
+                            <li class="meals-calendar timeslot" id="day7-slot2"></li>
                         </ul>
                         <ul class="meals-calendar-row">
                             <li class="timeslot-name evening">
                                 <i class="bi bi-moon-fill evening"></i>
                             </li>
-                            <li class="meals-calendar-border" id="day1-slot3"></li>
-                            <li class="meals-calendar-border" id="day2-slot3"></li>
-                            <li class="meals-calendar-border" id="day3-slot3"></li>
-                            <li class="meals-calendar-border" id="day4-slot3"></li>
-                            <li class="meals-calendar-border" id="day5-slot3"></li>
-                            <li class="meals-calendar-border" id="day6-slot3"></li>
-                            <li class="meals-calendar-border" id="day7-slot3"></li>
+                            <li class="meals-calendar timeslot" id="day1-slot3"></li>
+                            <li class="meals-calendar timeslot" id="day2-slot3"></li>
+                            <li class="meals-calendar timeslot" id="day3-slot3"></li>
+                            <li class="meals-calendar timeslot" id="day4-slot3"></li>
+                            <li class="meals-calendar timeslot" id="day5-slot3"></li>
+                            <li class="meals-calendar timeslot" id="day6-slot3"></li>
+                            <li class="meals-calendar timeslot" id="day7-slot3"></li>
                         </ul>
                     </div>
                 </div>
@@ -113,6 +114,7 @@ export default function Meals(props) {
 
 export function MealsEvent() {
     prepareSearchFields();
+    addTimeslotListeners();
     console.log("MealsEvent Complete");
 }
 
@@ -166,8 +168,40 @@ function populateResults() {
 function addResultListeners() {
 
 }
-
+function addTimeslotListeners() {
+    const timeslots = document.querySelectorAll(".timeslot");
+    timeslots.forEach(function(timeslot) {
+        timeslot.addEventListener("drop", function(e) {
+            e.preventDefault();
+            let data = e.dataTransfer.getData("text");
+            e.target.appendChild(document.getElementById(data));
+            console.log("data = " + data);
+        })
+        timeslot.addEventListener("dragover", function(e) {
+            e.preventDefault();
+            console.log("dragging");
+        })
+    })
+    let mealCards = document.querySelectorAll(".meal-card");
+    mealCards.forEach(function(mealCard) {
+        mealCard.addEventListener("dragstart", function(e) {
+            e.dataTransfer.setData("text", e.target.id);
+            console.log("Data is set.");
+        })
+    })
+}
 function searchFavoriteRecipes() {
 
 }
 
+function allowDrop(e) {
+    e.preventDefault();
+}
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+function drop(e) {
+    e.preventDefault();
+    let data = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(data));
+}
