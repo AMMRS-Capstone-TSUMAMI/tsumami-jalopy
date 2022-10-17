8fcaaac74f2a299d383a4be5fe98e61ef31811b3
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-10-16 21:46:51 -0500

    get map id

--- src/main/java/tsumami/tsumamijalopy/web/MealsController.java
+++ src/main/java/tsumami/tsumamijalopy/web/MealsController.java
@@ -1,14 +1,14 @@
 package tsumami.tsumamijalopy.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tsumami.tsumamijalopy.data.Meal;
 import tsumami.tsumamijalopy.data.MealsRepository;
 import tsumami.tsumamijalopy.data.Recipe;
 import tsumami.tsumamijalopy.data.RecipesRepository;
 
-import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
@@ -16,11 +16,13 @@ import java.util.Optional;
 @RequestMapping(value = "/api/meals", produces = "application/json")
 @CrossOrigin(origins = "http://localhost:8080/", exposedHeaders = "Content-Range")
 public class MealsController {
-//    private MealsRepository mealsRepository;
-//    private RecipesRepository recipesRepository;
+    private MealsRepository mealsRepository;
+    private RecipesRepository recipesRepository;
 
-//    @GetMapping("/meals/{id}")
-//    public ResponseEntity<Recipe>getRecipeById(PathVariable("id") Recipe id) {
+    @GetMapping("/meals/{id}")
+    public ResponseEntity<Recipe>getRecipeById(@PathVariable("id") Recipe id) {
+        Recipe entity = data.Recipe.getRecipeById(Recipe id);
 
-//    }
+        return new ResponseEntity<>(entity, new HttpHeaders(), HttpStatus.OK);
+    }
 }
