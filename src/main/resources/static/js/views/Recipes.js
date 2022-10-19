ea0a06e2fd39a582499ab54e853804aa7af1f736
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-10-19 16:23:26 -0500

    merge successful, functionality between meals.js and recipes.js

--- src/main/resources/static/js/views/Recipes.js
+++ src/main/resources/static/js/views/Recipes.js
@@ -8,13 +8,13 @@
 // let apiRecipeIngredients = [];
 // let apiCookingInstructions = [];
 
-let recipe;
+// let recipe;
 let recipeInfo;
 // let html;
 // let html2;
 
 export default function recipesHTML(props) {
-    recipe=props.recipes
+    recipeInfo=props.recipes
     // data = props.data;
     return `
          <main>
@@ -29,12 +29,13 @@ export default function recipesHTML(props) {
 export function recipesEvent() {
     // perhaps this is where I can call the recipe ID from meals.js
     //uncomment below to activate API call!! Only comment out if not wanting to automatically make the call
-    recipeSelectedHandler();
-    console.log("Hello recipesEvent");
+    // recipeSelectedHandler();
+    populateRecipeInfo();
+    // console.log("Hello recipesEvent");
     // console.log("${response.json}");
 }
 
-function recipeSelectedHandler(e) {
+// function recipeSelectedHandler(e) {
     // this is where I call the user ID from the other page
     // const userInput = document.querySelector('#search-bar');
     // userInput.addEventListener('keypress', function (e) {
@@ -46,47 +47,46 @@ function recipeSelectedHandler(e) {
     // let recipeID = 716429;
 
     //comment out line below after testing
-    getAPI(918034
-    );
+    // getAPI(918034);
 
     //uncomment below after testing
     // getAPI(recipe.id);
 
     // at this point getAPI will have finished
 
-    console.log("Hello recipeSelectedHandler");
+    // console.log("Hello recipeSelectedHandler");
     // alert("stuffs")
     // }
     // })
-}
+// }
 
-export function getAPI(userSelectedID) {
-    console.log("Hello getAPI");
-
-        // the ID below will instead be called recipeID and be pulled from meals.js
-    let SERVICE_URL = `https://api.spoonacular.com/recipes/`+userSelectedID+`/information`;
-    const request_url = SERVICE_URL + "?" + "apiKey=" + SPOONACULAR_API;
-    // let recipeSlot = [];
-    https://api.spoonacular.com/food/jokes/random + SPOONACULAR_API;
-    //request_url is our full api call url
-    recipeInfo  =
-        fetch(request_url).then(resp => {
-        //the resp is the data and below is the json format of the data from the api
-        return resp.json();
-        //food at this point is the usable json data. Can name whatever
-    }).then(food => {
-            console.log(food);
-            //? means look for this thing if exists as a key (needs further research)
-            console.log(food?.image);
-            // for (let i = 0; i < food.length; i++) {
-            //     recipeSlot.push(food.results)
-            // }
-            // console.log("logging recipeSlot here");
-            // console.log(recipeSlot)
-            recipeInfo = food;
-                populateRecipeInfo();
-        }
-        )}
+// export function getAPI(userSelectedID) {
+//     // console.log("Hello getAPI");
+//
+//         // the ID below will instead be called recipeID and be pulled from meals.js
+//     let SERVICE_URL = `https://api.spoonacular.com/recipes/`+userSelectedID+`/information`;
+//     const request_url = SERVICE_URL + "?" + "apiKey=" + SPOONACULAR_API;
+//     // let recipeSlot = [];
+//     https://api.spoonacular.com/food/jokes/random + SPOONACULAR_API;
+//     //request_url is our full api call url
+//     recipeInfo  =
+//         fetch(request_url).then(resp => {
+//         //the resp is the data and below is the json format of the data from the api
+//         return resp.json();
+//         //food at this point is the usable json data. Can name whatever
+//     }).then(food => {
+//             console.log(food);
+//             //? means look for this thing if exists as a key (needs further research)
+//             console.log(food?.image);
+//             // for (let i = 0; i < food.length; i++) {
+//             //     recipeSlot.push(food.results)
+//             // }
+//             // console.log("logging recipeSlot here");
+//             // console.log(recipeSlot)
+//             recipeInfo = food;
+//                 populateRecipeInfo();
+//         }
+//         )}
 
     function populateRecipeInfo(){
 
