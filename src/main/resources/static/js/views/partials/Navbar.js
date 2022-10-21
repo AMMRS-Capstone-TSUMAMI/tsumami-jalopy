65f2f9d82b0d5756a6c7bb200f9f2e434ebb1373
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-10-21 09:40:09 -0500

    logo worrking

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
 
         return `
             <nav id="navbar" class="d-flex bd-highlight mb-3 mt-1">
-                <a id="nav-logo" href="/about"><img src="/img/logo-sm.png" alt="img"></a>
+                <a id="nav-logo" href="/about"><img src="/img/logo-sm.png" data-passthru alt="img"></a>
                 <a href="/meals" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Meals</a>
                 <a href="/me" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Me</a>
                 <a href="/logout" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Logout</a>
@@ -14,17 +14,7 @@ export default function Navbar(props) {
     `;
 
 
-
     } else {
         return ""
     }
-
-
-}
-
-export function clickLogo() {
-    let navLogo = document.querySelector("#nav-logo");
-    navLogo.addEventListener("click", function () {
-        window.location = "/about"
-    })
 }
