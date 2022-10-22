df632abdb83aefd7cbec3f2e194bd18b6e61651a
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-10-22 15:25:45 -0500

    added code to index.css to nullify bounce effect when scrolling to top or bottomn of pages

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -86,7 +86,7 @@ export default function router(URI) {
             // backgroundImage: "url('https://demos.creative-tim.com/paper-kit-2/assets/img/antoine-barres.jpg');"
             // backgroundImage: "url('\img/norway_fjord_2000x1200.jpeg\');",
             // backgroundImage: "url('\img/ocean-bgResized.jpeg\');",
-            backgroundImage: "url('\img/landingPageBg.jpeg\');",
+            backgroundImage: "url('\img/ocean-bgResized.jpeg\');",
             backdropFilter:  `blur(7.7px)`,
             viewEvent: aboutEvent
         },
