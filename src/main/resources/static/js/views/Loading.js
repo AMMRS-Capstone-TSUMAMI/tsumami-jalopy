8ad50ee99bb2d9ef95205475e5d86bd6e0a66f79
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-10-23 17:08:50 -0500

    simplified loading screen

--- src/main/resources/static/js/views/Loading.js
+++ src/main/resources/static/js/views/Loading.js
@@ -1,21 +1,9 @@
 export default function Loading(props) {
     return `
-    <div class="load">
-        <div class="loading">
-            <div class="loadTip">
-                <h4>DEV TIP NO. 15</h4>
-            </div>
-            <div class="loadHeader">
-                <p>When in doubt, sout it out.</p>
-            </div>
+        <div id="loader-wrapper">
+            <div id="loader"></div>            
+            <div class="loader-section section-left"></div>
+            <div class="loader-section section-right"></div>
         </div>
-        <div class="loadBar">
-            <p style="text-align: center">INITIALIZING RESPAWN_SEQUENCE.BAT...</p>
-            <div class="progress-bar">
-                <span class="progress-bar__inner" style="width: 100%"></span>
-            </div>
-            <p style="text-align: center">PREDICTING CHANCE OF WEBSITE CRASH...</p>
-        </div>
-    </div>
 `;
 }
