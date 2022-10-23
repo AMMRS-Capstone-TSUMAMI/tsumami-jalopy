27b7646b1da60abb51d0e1966556051196782edd
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-10-23 10:45:06 -0500

    added dropdown for smaller screens

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -8,6 +8,9 @@ export default function Navbar(props) {
         <a class="navbar-brand" data-link href="/about">
             <img src="/img/logo-dark-sm.png" data-passthru alt="tsUmami" height="40" class="d-inline-block align-text-top">
         </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
