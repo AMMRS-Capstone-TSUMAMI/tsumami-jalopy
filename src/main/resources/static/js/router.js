de8088c0013f3d64bd982461b42daaa18d5090d7
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-10-23 15:42:08 -0500

    set dark background for meals and recipes

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -71,7 +71,8 @@ export default function router(URI) {
             },
             uri: '/meals',
             title: 'Meals',
-            viewEvent: MealsEvent
+            viewEvent: MealsEvent,
+            backgroundColor: `var(--bg-color-dark)`
         },
         '/about': {
             returnView: About,
@@ -109,7 +110,8 @@ export default function router(URI) {
             },
             uri: '/recipes/:id',
             title: 'Recipes',
-            viewEvent: recipesEvent
+            viewEvent: recipesEvent,
+            backgroundColor: `var(--bg-color-dark)`
         }
     };
 
