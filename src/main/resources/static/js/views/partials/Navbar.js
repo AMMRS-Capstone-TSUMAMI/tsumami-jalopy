6da70c6a0de727a3c821fa96774e496e2fcf2cdf
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-10-23 22:17:27 -0500

    style makeover

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -6,21 +6,21 @@ export default function Navbar(props) {
 <nav class="navbar navbar-expand-sm">
     <div class="container-fluid">
         <a class="navbar-brand" data-link href="/about">
-            <img src="/img/logo-dark-sm.png" data-passthru alt="tsUmami" height="40" class="d-inline-block align-text-top">
+            <img src="/img/logo-sm.png" data-passthru alt="tsUmami" height="40" class="d-inline-block align-text-top">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
                 <li class="nav-item">
-                    <a class="nav-link active" data-link aria-current="true" href="/meals">Meal Planner</a>
+                    <a class="nav-link active" style="color: white" data-link aria-current="true" href="/meals">Meal Planner</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" data-link href="/me">My Account</a>
+                    <a class="nav-link active" style="color: white" data-link href="/me">My Account</a>
                 </li> 
                 <li class="nav-item">
-                    <a class="nav-link active" data-link href="/logout">Logout</a>
+                    <a class="nav-link active" style="color: white" data-link href="/logout">Logout</a>
                 </li>
             </ul>
         </div>
