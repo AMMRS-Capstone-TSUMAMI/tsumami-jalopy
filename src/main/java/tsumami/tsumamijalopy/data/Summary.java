da3e2ed6e25cd7c6aa0dad3af9dd05c794421b1f
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-10-25 10:55:58 -0500

    deleted Summary.java

--- src/main/java/tsumami/tsumamijalopy/data/Summary.java
+++
@@ -1,9 +0,0 @@
-package tsumami.tsumamijalopy.data;
-
-public interface Summary {
-        Long getDayNum();
-        Long getCalories();
-        Long getFat();
-        Long getCarbs();
-        Long getProtein();
-}
